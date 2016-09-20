c3dd0975ac57a9add8505d88efb0e34a5651e402
mgruben <GrubenM@gmail.com>
2016-09-20 08:58:16 -0500

    Initialize solution file with isPrime function

---
+++ QuadraticPrimes.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+ // https://en.wikipedia.org/wiki/Primality_test#Pseudocode
+bool isPrime(int n) { 
+    if (n <= 1) return false;
+    if (n <= 3) return true;
+    if ((n % 2 == 0) || (n % 3 == 0)) return false;
+    int i = 5;
+    while (i * i < n) {
+        if ((n % i == 0) || (n % (i + 2) == 0)) return false;
+        i += 6;
+    }
+    return true;
+}
+
+int main() {
+    return 0;
+}
